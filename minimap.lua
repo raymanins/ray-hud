1ebb106319a95518754dcc6f4a425291ee14d08a
raymans <145383215+raymanins@users.noreply.github.com>
2024-05-19 16:56:08 +0300

    Add files via upload

--- minimap.lua
+++ minimap.lua
@@ -108,25 +108,36 @@ function SetAirAmount(amount)
     EndScaleformMovieMethod()
 end
 
-RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
-AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
-    Citizen.CreateThread(function()
-        MinimapScaleform.scaleform = RequestScaleformMovie("minimap")
-        SetRadarBigmapEnabled(true, false)
-        Wait(0)
-        SetRadarBigmapEnabled(false, false)
-    end)
+Citizen.CreateThread(function()
+    MinimapScaleform.scaleform = RequestScaleformMovie("minimap")
+    SetRadarBigmapEnabled(true, false)
+    Wait(0)
+    SetRadarBigmapEnabled(false, false)
+end)
 
-    Citizen.CreateThread(function()
-        local minimap = RequestScaleformMovie("minimap")
-        SetRadarBigmapEnabled(true, false)
-        Wait(0)
-        SetRadarBigmapEnabled(false, false)
-        while true do
-            Wait(0)
-            BeginScaleformMovieMethod(minimap, "SETUP_HEALTH_ARMOUR")
+Citizen.CreateThread(function()
+    MinimapScaleform.scaleform = RequestScaleformMovie("minimap")
+    SetRadarBigmapEnabled(true, false)
+    Wait(0)
+    SetRadarBigmapEnabled(false, false)
+    
+    while true do
+        Citizen.Wait(0)
+        
+        local ped = PlayerPedId()
+        local vehicle = GetVehiclePedIsIn(ped, false)
+        
+        -- Check if the player is in a vehicle
+        if IsPedInAnyVehicle(ped, false) then
+            -- Only display minimap when in a vehicle
+            BeginScaleformMovieMethod(MinimapScaleform.scaleform, "SETUP_HEALTH_ARMOUR")
             ScaleformMovieMethodAddParamInt(3)
             EndScaleformMovieMethod()
+        else
+            -- Hide minimap when not in a vehicle
+            BeginScaleformMovieMethod(MinimapScaleform.scaleform, "SETUP_HEALTH_ARMOUR")
+            ScaleformMovieMethodAddParamInt(0)
+            EndScaleformMovieMethod()
         end
-    end)
+    end
 end)
